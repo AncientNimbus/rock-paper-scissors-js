704982b6a2b60aeee6b95bc448be1e9a74e177d8
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-04-30 03:40:35 +0100

    chore: Add pseudocode to describe the program flow

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,25 @@
-console.log("Hello World");
+// Rock Paper Scissors v1.0 Console Edition
+/**
+ * Program Flow
+ * Create an array that contain 3 string value: rock, paper and scissors
+ * Create a function that randomly pick a value from the array
+ * This value will be stored as computer's choice
+ * Create a prompt that will collect user's choice
+ * Check user's choice against the array to ensure the option is valid
+ * Initialized user's score and computer's score
+ * Compare computer's choice against user's choice
+ * If user win, add a point to the user
+ * If user lose, add a point to the computer
+ * If it is a tie, restart the round
+ * The first to reach 5 points win the game
+ */
+
+// TODO: Get computer's choice
+
+// TODO: Get user's choice
+
+// TODO: Store user's score
+
+// TODO: Play a single round
+
+// TODO: Play the entire game (5 rounds)
